2478e091472e9ac16b3177676732a67fdf476604
arianerousseau <ariane.rousseau@efrei.net>
2022-10-10 18:53:25 -0700

    Second commit

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 var express = require('express');
 var app = express();
+const path = require("path");
 const port = 3000;
+//const jsonFile = fetch('https://cat-fact.herokuapp.com/facts')
+//import myJson from 'data.json' assert {type: 'json'};
+//import { json, response } from 'express';
 
 app.use(express.static('public'));
 
+const jsonData= require('./data.json'); 
+
+function nbFact(number){
+    return number;
+}
+
+app.get('')
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/getFact', function(req, res){
+    console.log("xa marche");
+    console.log(req.query);
+    var reqJson = req.query.number;
+    //-------------------------
+    /*
+    var result = '"data":[';
+    for(i =0; i<reqJson;i++){
+        result+= '{"fact":' + '"' + jsonData.data[i].fact + '"},';
+    }
+    result = result.substr(0, result.length - 1);
+    result+="]";
+    console.log("Sending : " + result);
+    res.send(JSON.stringify(result));
+    */
+});
+
 app.listen(port, function(){
-    console.log('Example app listening on port ${port}!');
+    console.log(`Example app listening on port ${port}!`);
 });
--- public/css/script.js
+++ public/css/script.js
@@ -1,12 +1,30 @@
-const btn = document.getElementById("btnID");
-console.log(btn);
-btn.addEventListener("click", show);
+const btnCat = document.getElementById("btnCat");
+btnCat.addEventListener("click", factCat);
 
-function show() {
-    console.log('test');
-    document.getElementById('image')
-            .style.display = "block";
+function factCat(){
+    console.log('button pressed');
+    var value = document.getElementById("num_facts").value;
 
-    document.getElementById('btnID')
-            .style.display = "none";
+    if(value != ""){
+        var xhttp = new XMLHttpRequest();
+        const param = `number=${value}`;
+        xhttp.open("GET", "/getFact?" + param, true);
+        xhttp.send();
+        console.log("Ajax request sent: "+param);
+
+        xhttp.addEventListener('load', function(){
+            if (xhttp.status === 200 && xhttp.readyState === 4){
+                var response = JSON.parse(xhttp.responseText);
+                console.log(response);
+                var toPrint = "";
+                for(i=0; i<value; i++){
+                    toPrint += "\n";
+                }
+                document.getElementById("paragraphe").innerHTML = response;
+        }else {
+                console.error("Bad request");
+            }
+        })
+
+    }
 }
